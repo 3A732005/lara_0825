043d1b25400d04f94dc8c65a59da44fdb99b51f5
li ya hsuan <ab53014698@yahoo.com.tw>
2020-11-11 10:37:40 +0800

    改變CRUD位置(/)+課堂練習

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,59 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+
+    //新增資料
+    /*\App\Models\Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);*/
+
+//    $post=new\App\Models\Post();
+//    $post->title='test title';
+//    $post->content='test content';
+//    $post->save();
+
+//查詢資料
+    /*$posts=\App\Models\Post::all();
+    dd($posts);*/
+
+    /*$post=\App\Models\Post::find(1);
+    dd($post);*/
+
+    /*$posts=\App\Models\Post::where('id','<',10)->orderBy('id','DESC')->get();
+    dd($posts);*/
+
+//更新資料
+    /*$post=\App\Models\Post::find(1);
+    $post->update([
+        'title'=>'updated title',
+        'content'=>'updated content',
+    ]);*/
+
+    /*$post=\App\Models\Post::find(1);
+    $post->title='save dtitle';
+    $post->content='save dcontent';
+    $post->save();*/
+
+//刪除資料
+    /*$post=\App\Models\Post::find(1);
+    $post->delete();*/
+
+    /*\App\Models\Post::destroy(2);*/
+
+    /*\App\Models\Post::destroy(3,5,7);*/
+
+//了解 Model 和 Collection 的差異
+    $allPosts=\App\Models\Post::all();
+    dd($allPosts);
+    /*$featuredPosts=\App\Models\Post::where('is_feature',1)->get();
+    dd($featuredPosts);*/
+
+    /*$fourthPost=\App\Models\Post::find(4);
+    dd($fourthPost);*/
+    /*$lastPost=\App\Models\Post::orderBy('id','DESC')->first();
+    dd($lastPost);*/
 });
 
 Route::get('posts',[PostsController::class, 'index'])->name('posts.index');
@@ -26,55 +78,5 @@ Route::get('about',[PostsController::class, 'about'])->name('posts.about');
 
 Route::get('contact',[PostsController::class, 'contact'])->name('posts.contact');
 
-//新增資料
-/*\App\Models\Post::create([
-    'title'=>'test title',
-    'content'=>'test content',
-]);*/
 
-/*$post=new\App\Models\Post();
-$post->title='test title';
-$post->content='test content';
-$post->save();*/
-
-//查詢資料
-/*$posts=\App\Models\Post::all();
-dd($posts);*/
-
-/*$post=\App\Models\Post::find(1);
-dd($post);*/
-
-/*$posts=\App\Models\Post::where('id','<',10)->orderBy('id','DESC')->get();
-dd($posts);*/
-
-//更新資料
-/*$post=\App\Models\Post::find(1);
-$post->update([
-    'title'=>'updated title',
-    'content'=>'updated content',
-]);*/
-
-/*$post=\App\Models\Post::find(1);
-$post->title='save dtitle';
-$post->content='save dcontent';
-$post->save();*/
-
-//刪除資料
-/*$post=\App\Models\Post::find(1);
-$post->delete();*/
-
-/*\App\Models\Post::destroy(2);*/
-
-/*\App\Models\Post::destroy(3,5,7);*/
-
-//了解 Model 和 Collection 的差異
-/*$allPosts=\App\Models\Post::all();
-dd($allPosts);*/
-/*$featuredPosts=\App\Models\Post::where('is_feature',1)->get();
-dd($featuredPosts);*/
-
-/*$fourthPost=\App\Models\Post::find(4);
-dd($fourthPost);*/
-$lastPost=\App\Models\Post::orderBy('id','DESC')->first();
-dd($lastPost);
 
